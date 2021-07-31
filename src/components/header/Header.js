b31e2114b604c07e10c02090f6ea024d26a47c74
JoviRC <johanrc95@gmail.com>
2021-07-30 20:49:59 -0700

    Nav- Header- animations

---
+++ src/components/header/Header.js
@@ -0,0 +1,15 @@
+import * as React from "react";
+import "./Header.css";
+
+const Header = () => {
+  return (
+    <div className="header__content">
+      <div className="header__titulo">
+        <h1 className="header__h1">Johan</h1>
+        <h1 className="header__h1 header__h1--last">Rivera</h1>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
--- src/pages/MainPage.js
+++ src/pages/MainPage.js
@@ -1,4 +1,5 @@
 import React from "react";
+import Header from "../components/header/Header";
 import NavBar from "../components/navbar/NavBar";
 import "./MainPage.css"
 
@@ -6,6 +7,7 @@ const MainPage = () => {
   return (
     <div className="container">
       <NavBar />
+      <Header />
     </div>
   )
 };
